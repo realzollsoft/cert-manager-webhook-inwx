8573e1a8e2d89a7bdbb8968a50e89cff8016067d
Patrick Thum <4435508+pthum@users.noreply.github.com>
2026-03-13 10:17:28 +0100

    harden fetch-test-binaries script and add x64 handling

--- scripts/fetch-test-binaries.sh
+++ scripts/fetch-test-binaries.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m)
 ver="v1.33.0"
+
+case "${arch}" in
+    x86_x64*)     arch="amd64";;
+esac
+
 curl -sSL  "https://github.com/kubernetes-sigs/controller-tools/releases/download/envtest-${ver}/envtest-${ver}-${os}-${arch}.tar.gz" | tar -zvxf -
 rm -rf kubebuilder || true
 mv controller-tools kubebuilder
