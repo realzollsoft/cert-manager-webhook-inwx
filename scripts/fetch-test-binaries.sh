a99d22bc33d66d1b5a531ab2e9a1f61039584aec
Patrick Thum <4435508+pthum@users.noreply.github.com>
2026-03-13 08:47:53 +0100

    fix fetch-test-binaries script

--- scripts/fetch-test-binaries.sh
+++ scripts/fetch-test-binaries.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-os=$(uname -s)
-curl -sSL  https://storage.googleapis.com/kubebuilder-tools/kubebuilder-tools-1.30.0-"$(echo "$os" | tr '[:upper:]' '[:lower:]')"-amd64.tar.gz | tar -zvxf -
-
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
+arch=$(uname -m)
+ver="v1.33.0"
+curl -sSL  "https://github.com/kubernetes-sigs/controller-tools/releases/download/envtest-${ver}/envtest-${ver}-${os}-${arch}.tar.gz" | tar -zvxf -
+rm -rf kubebuilder || true
+mv controller-tools kubebuilder
+mv kubebuilder/envtest kubebuilder/bin
