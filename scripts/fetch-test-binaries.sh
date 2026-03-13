d841426df0dde95ce8d1f183f920be20b9fdbb13
Patrick Thum <4435508+pthum@users.noreply.github.com>
2026-03-13 10:21:16 +0100

    fix typo in fetch-test-binaries script

--- scripts/fetch-test-binaries.sh
+++ scripts/fetch-test-binaries.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m)
 ver="v1.33.0"
 
 case "${arch}" in
-    x86_x64*)     arch="amd64";;
+    x86_64*)     arch="amd64";;
 esac
 
 curl -sSL  "https://github.com/kubernetes-sigs/controller-tools/releases/download/envtest-${ver}/envtest-${ver}-${os}-${arch}.tar.gz" | tar -zvxf -
